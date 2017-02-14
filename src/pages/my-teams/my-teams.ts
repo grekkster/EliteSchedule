a63c2bd9d24bb038e351f3c1f19e13dd4c0becb7
jura <jirapanek@gmail.com>
2017-02-14 22:28:54 +0100

    MyTeams added and functional.

---
+++ src/pages/my-teams/my-teams.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/*
+  Generated class for the MyTeams page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-my-teams',
+  templateUrl: 'my-teams.html'
+  //templateUrl: 'build/pages/my-teams/my-teams.html'
+})
+export class MyTeamsPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad MyTeamsPage');
+  }
+
+}
---
+++ src/pages/pages.ts
@@ -0,0 +1,1 @@
+export * from './my-teams/my-teams';
